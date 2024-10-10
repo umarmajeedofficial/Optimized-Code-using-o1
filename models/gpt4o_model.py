4b598bebedb2e74ef6b82d960390054563949ded
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-10 10:27:12 +0500

    Update and rename code_llama.py to gpt4o_model.py

--- models/gpt4o_model.py
+++ models/gpt4o_model.py
@@ -1,15 +1,19 @@
-# models/code_llama.py
+# models/gpt4o_model.py
 
 from openai import OpenAI
 
-class CodeLlamaModel:
+class GPT4oModel:
     def __init__(self, api_key, base_url="https://api.aimlapi.com"):
         self.client = OpenAI(api_key=api_key, base_url=base_url)
 
     def generate_code(self, instruction):
         response = self.client.chat.completions.create(
-            model="codellama/CodeLlama-70b-hf",
+            model="gpt-4o",
             messages=[
+                {
+                    "role": "system",
+                    "content": "You are an AI assistant who knows everything."
+                },
                 {
                     "role": "user",
                     "content": instruction
@@ -21,8 +25,12 @@ class CodeLlamaModel:
 
     def explain_code(self, instruction):
         response = self.client.chat.completions.create(
-            model="codellama/CodeLlama-70b-hf",
+            model="gpt-4o",
             messages=[
+                {
+                    "role": "system",
+                    "content": "You are an AI assistant who knows everything."
+                },
                 {
                     "role": "user",
                     "content": instruction
