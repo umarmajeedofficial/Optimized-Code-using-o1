d6012e7f92dc51f8d2fdb496179aca46dddec346
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-10 09:22:36 +0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,5 @@
 from .o1_preview import O1PreviewModel
 from .o1_mini import O1MiniModel
 from .llama import LlamaModel
-from .codellama_70b import CodeLlama70BModel
-from .deepseek_coder_33b import DeepseekCoder33BModel
-from .wizardcoder_python_34b import WizardCoderPython34BModel
-from .phind_codellama_v2 import PhindCodeLlamaV2Model
-from .sqlcoder_15b import SQLCoder15BModel
-
+from .deepseek_coder_instruct import DeepseekCoderInstructModel
+from .code_llama import CodeLlamaModel
