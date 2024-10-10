03703aec1d2aa7a9706274b17413d71e40c415de
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-10 14:44:09 +0500

    Update gpt4o_model.py

--- models/gpt4o_model.py
+++ models/gpt4o_model.py
@@ -19,7 +19,7 @@ class GPT4oModel:
                     "content": instruction
                 },
             ],
-            max_tokens=512,
+            max_tokens=10000,
         )
         return response.choices[0].message.content.strip()
 
@@ -36,6 +36,6 @@ class GPT4oModel:
                     "content": instruction
                 }
             ],
-            max_tokens=512,
+            max_tokens=10000,
         )
         return response.choices[0].message.content.strip()
