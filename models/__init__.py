1db14599a4f177d80125ff184bec7e2c7aa324c9
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-10 11:01:33 +0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,4 +4,4 @@ from .o1_preview import O1PreviewModel
 from .o1_mini import O1MiniModel
 from .llama import LlamaModel
 from .deepseek_coder_instruct import DeepseekCoderInstructModel
-from .gpt4o import Gpt4oModel
+from .gpt4o_model import Gpt4oModel
