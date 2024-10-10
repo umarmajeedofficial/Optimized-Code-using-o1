3099f8682f316473b0224743c51469c4eaa01168
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-10 11:20:45 +0500

    Update and rename deepseek_coder_instruct.py to gemini_model.py

--- models/gemini_model.py
+++ models/gemini_model.py
@@ -1,14 +1,14 @@
-# models/deepseek_coder_instruct.py
+# models/gemini_model.py
 
 from openai import OpenAI
 
-class DeepseekCoderInstructModel:
+class GeminiModel:
     def __init__(self, api_key, base_url="https://api.aimlapi.com"):
         self.client = OpenAI(api_key=api_key, base_url=base_url)
 
     def generate_code(self, instruction):
         response = self.client.chat.completions.create(
-            model="deepseek-ai/deepseek-coder-33b-instruct",
+            model="gemini-1.5-pro",
             messages=[
                 {
                     "role": "user",
@@ -21,7 +21,7 @@ class DeepseekCoderInstructModel:
 
     def explain_code(self, instruction):
         response = self.client.chat.completions.create(
-            model="deepseek-ai/deepseek-coder-33b-instruct",
+            model="gemini-1.5-pro",
             messages=[
                 {
                     "role": "user",
